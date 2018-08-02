3bc2470d93ea5b5092050fe3eddab5fc4464eba6
mithrilhall <eric_estes@aspensquare.com>
2018-08-02 16:04:41 -0400

    Added init and the test action that sets the property.

--- app/components/property-selector/component.js
+++ app/components/property-selector/component.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    init() {
+        this._super(...arguments);
+    },
+
+    actions: {
+        test(property) {
+            this.set('property', property);
+            this.setProperty(property);
+        }
+    }
 });
