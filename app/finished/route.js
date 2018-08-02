89ecb83999d3c1954908c4d5767ebc804159a9db
mithrilhall <eric_estes@aspensquare.com>
2018-08-02 16:06:16 -0400

    Added the afterModel hook to open the page in the default browser.

--- app/finished/route.js
+++ app/finished/route.js
@@ -2,8 +2,13 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params) {
-        {
-            return params.name;
+        return params;
+    },
+
+    afterModel(model) {
+        if (model && model.pageToVisit) {
+            const { shell } = window.requireNode('electron');
+            shell.openExternal(model.pageToVisit);
         }
     }
 });
